e1a0d84151289eae224794cb1f7ce93557d95bb0
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-07-04 16:18:25 +0000

    基础代码

--- code/HouseCalculator/src/com/rayboot/housecalculator/MainActivity.java
+++ code/HouseCalculator/src/com/rayboot/housecalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.rayboot.housecalculator;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.Collections;
 import java.util.List;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -12,12 +13,20 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import butterknife.InjectView;
 import butterknife.Views;
@@ -37,29 +46,54 @@ public class MainActivity extends SherlockActivity {
 	final int MORE_SHARE = 4;
 	final String rateUrl = "http://wxcs.ahhouse.com/yiDong/oldhouse.php/Index/index/type/rate/ps/100/isXml/1/";
 	List<RateObj> rateList;
+	String[] mRateStrings = null;
+	final String[] yearArrays = new String[] { "2年（24期）", "3年（36期）", "4年（48期）",
+			"5年（60期）", "6年（72期）", "7年（84期）", "8年（96期）", "9年（108期）",
+			"10年（120期）", "15年（180期）", "20年（240期）", "25年（300期）", "30年（360期）" };
+	@InjectView(R.id.spType)
 	Spinner spType;
+	@InjectView(R.id.llCustomRate)
+	LinearLayout llCustomRate;
+	@InjectView(R.id.spRate)
+	Spinner spRate;
+	@InjectView(R.id.tvRateDetail)
+	TextView tvRateDetail;
+	@InjectView(R.id.tvSelectYearGJJ)
+	TextView tvSelectYearGJJ;
+	@InjectView(R.id.tvSelectYearSY)
+	TextView tvSelectYearSY;
+	@InjectView(R.id.etYearGJJ)
+	EditText etYearGJJ;
+	@InjectView(R.id.etYearSY)
+	EditText etYearSY;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setTheme(R.style.MyTheme);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Views.inject(this);
 		Utilly.initUMeng(this);
 		getRateData();
-		
-		spType = (Spinner) findViewById(R.id.spType);
-		
+
+		tvSelectYearGJJ.setOnClickListener(onClickListener);
+		tvSelectYearSY.setOnClickListener(onClickListener);
+
 		spType.setOnItemSelectedListener(new OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> parent, View view,
 					int position, long id) {
-				String str = parent.getItemAtPosition(position).toString();
-				Toast.makeText(MainActivity.this, "你点击的是:" + str + "  position " + position + "  id  " + id, 2000)
-						.show();
-				if (position == 0) {
-					
-				}else {
-					
+				switch (position) {
+				case 0:
+					llCustomRate.setVisibility(View.GONE);
+					spRate.setVisibility(View.VISIBLE);
+					tvRateDetail.setVisibility(View.VISIBLE);
+					break;
+				case 1:
+					llCustomRate.setVisibility(View.VISIBLE);
+					spRate.setVisibility(View.GONE);
+					tvRateDetail.setVisibility(View.GONE);
+					break;
 				}
 			}
 
@@ -68,7 +102,36 @@ public class MainActivity extends SherlockActivity {
 				// TODO Auto-generated method stub
 			}
 		});
+		spRate.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				tvRateDetail.setText(rateList.get(position).toString());
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				// TODO Auto-generated method stub
+			}
+		});
 	}
+
+	private OnClickListener onClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch (v.getId()) {
+			case R.id.tvSelectYearGJJ:
+			case R.id.tvSelectYearSY:
+				showSelectYearDialog(v);
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -122,7 +185,40 @@ public class MainActivity extends SherlockActivity {
 		MobclickAgent.onPause(this);
 	}
 
+	private void showSelectYearDialog(final View v) {
+		if (v.getTag() == null) {
+			v.setTag(0);
+		}
+		Dialog alertDialog = new AlertDialog.Builder(this)
+				.setTitle("请选择贷款年限")
+				.setIcon(R.drawable.ic_launcher)
+				.setSingleChoiceItems(yearArrays, (Integer) v.getTag(),
+						new DialogInterface.OnClickListener() {
+
+							@Override
+							public void onClick(DialogInterface dialog,
+									int which) {
+								v.setTag(which);
+								dialog.dismiss();
+								String temp = yearArrays[which];
+								temp = temp.substring(temp.indexOf("（") + 1,
+										temp.indexOf("期"));
+								if (v.getId() == R.id.tvSelectYearGJJ) {
+									etYearGJJ.setText(temp);
+								} else if (v.getId() == R.id.tvSelectYearSY) {
+									etYearSY.setText(temp);
+								}
+							}
+						}).create();
+		alertDialog.show();
+	}
+
 	private void getRateData() {
+		// 没连接网络
+		if (rateList == null) {
+			rateList = parse(Utilly.getFromAssets(this, "rate"));
+			initRateSelect();
+		}
 		if (Utilly.IsHaveInternet(this)) {
 			// 连接网络
 			AsyncHttpClient client = new AsyncHttpClient();
@@ -140,13 +236,32 @@ public class MainActivity extends SherlockActivity {
 					super.onSuccess(arg0);
 
 					rateList = parse(arg0);
+					initRateSelect();
 				}
 
 			});
-		} else {
-			// 没连接网络
-			rateList = parse(Utilly.getFromAssets(this, "rate"));
 		}
+	}
+
+	private void initRateSelect() {
+		Collections.reverse(rateList);
+		if (rateList != null && rateList.size() > 0) {
+			mRateStrings = new String[rateList.size()];
+			for (int i = 0; i < rateList.size(); i++) {
+				if (rateList.get(i).name.contains("selected>")) {
+					rateList.get(i).name = rateList.get(i).name.replace(
+							"selected>", "");
+				}
+				mRateStrings[i] = rateList.get(i).name;
+			}
+		}
+		ArrayAdapter<String> rateAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_spinner_item, mRateStrings);
+		rateAdapter
+				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spRate.setAdapter(rateAdapter);
+		tvRateDetail.setText(rateList.get(spRate.getSelectedItemPosition())
+				.toString());
 	}
 
 	public List<RateObj> parse(String xmlString) {
--- code/HouseCalculator/src/com/rayboot/housecalculator/RateObj.java
+++ code/HouseCalculator/src/com/rayboot/housecalculator/RateObj.java
@@ -1,5 +1,7 @@
 package com.rayboot.housecalculator;
 
+import java.text.DecimalFormat;
+
 public class RateObj {
 	public String name;
 	public RateDetailObj year1;
@@ -16,4 +18,20 @@ public class RateObj {
 		year6 = new RateDetailObj();
 	}
 
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return name + "  明细如下\n" + year1.name + "  公积金：" + getDouble(year1.gjj)
+				+ "%  商贷：" + getDouble(year1.sy) + "%\n" + year2.name
+				+ "  公积金：" + getDouble(year2.gjj) + "%  商贷："
+				+ getDouble(year2.sy) + "%\n" + year4.name + "  公积金："
+				+ getDouble(year4.gjj) + "%  商贷：" + getDouble(year4.sy) + "%\n"
+				+ year6.name + "  公积金：" + getDouble(year6.gjj) + "%  商贷："
+				+ getDouble(year6.sy) + "%";
+	}
+
+	private double getDouble(String value) {
+		DecimalFormat df = new DecimalFormat("#.##");
+		return Double.parseDouble(df.format(Double.valueOf(value) * 100));
+	}
 }
