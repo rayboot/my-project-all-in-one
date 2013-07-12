1817abcc47712881ee89f8cd065ebc21028cf7ae
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-07-12 06:01:03 +0000

    youhua

---
+++ code/WeatherPK/src/com/rayboot/weatherpk/MainActivity.java
@@ -0,0 +1,25 @@
+package com.rayboot.weatherpk;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        //http://blog.mynook.info/2012/08/18/weather-com-cn-api.html
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
