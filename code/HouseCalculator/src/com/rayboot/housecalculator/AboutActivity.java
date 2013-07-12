162bcdcdf15227ce1f0224dff65f0c9debe0ef89
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-07-12 04:03:39 +0000

    youhua

--- code/HouseCalculator/src/com/rayboot/housecalculator/AboutActivity.java
+++ code/HouseCalculator/src/com/rayboot/housecalculator/AboutActivity.java
@@ -1,22 +1,34 @@
 package com.rayboot.housecalculator;
 
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.widget.TextView;
+import butterknife.InjectView;
+import butterknife.Views;
 
-public class AboutActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class AboutActivity extends SherlockActivity {
+	@InjectView(R.id.tvAbout)
+	TextView tvAbout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.MyTheme);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about);
-	}
+		Views.inject(this);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.about, menu);
-		return true;
+		String str = "欢迎使用 " + getString(R.string.app_name);
+		try {
+			str = String.format(this.getString(R.string.about_content),
+					getString(R.string.app_name), getPackageManager()
+							.getPackageInfo(getPackageName(), 0).versionName);
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		tvAbout.setText(str);
 	}
 
 }
