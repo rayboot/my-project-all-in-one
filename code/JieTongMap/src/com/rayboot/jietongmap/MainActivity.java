ba0e77256f0346601f75bd12b968917459b980d4
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-08-18 14:28:52 +0000

    基础代码

--- code/JieTongMap/src/com/rayboot/jietongmap/MainActivity.java
+++ code/JieTongMap/src/com/rayboot/jietongmap/MainActivity.java
@@ -6,12 +6,39 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 import butterknife.InjectView;
 import butterknife.Views;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.LocationData;
+import com.baidu.mapapi.map.MapController;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationOverlay;
+import com.baidu.platform.comapi.basestruct.GeoPoint;
+
 public class MainActivity extends Activity {
+	// 定位相关
+	LocationClient mLocClient;
+	LocationData locData = null;
+	public MyLocationListenner myLocationListener = new MyLocationListenner();
+	// 定位图层
+	MyLocationOverlay myLocationOverlay = null;
+	boolean isRequest = false;// 是否手动触发请求定位
+	boolean isFirstLoc = true;// 是否首次定位
+
+	@InjectView(R.id.bmapView)
+	MapView mMapView = null; // 地图View
+	private MapController mMapController = null;
+
 	@InjectView(R.id.btnInputActivity)
 	Button btnInputActivity;
+	@InjectView(R.id.ivLoc)
+	ImageView ivLoc;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +51,108 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				MainActivity.this.startActivity(new Intent(MainActivity.this, SearchMapActivity.class));
+				MainActivity.this.startActivity(new Intent(MainActivity.this,
+						SearchMapActivity.class));
 			}
 		});
+		ivLoc.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				requestLocClick();
+			}
+		});
+
+		mMapController = mMapView.getController();
+		mMapView.getController().setZoom(14);
+		mMapView.getController().enableClick(true);
+		mMapView.setBuiltInZoomControls(true);
+
+		// 定位初始化
+		mLocClient = new LocationClient(this);
+		locData = new LocationData();
+		mLocClient.registerLocationListener(myLocationListener);
+		LocationClientOption option = new LocationClientOption();
+		option.setOpenGps(true);// 打开gps
+		option.setCoorType("bd09ll"); // 设置坐标类型
+		option.setScanSpan(5000);
+		mLocClient.setLocOption(option);
+		mLocClient.start();
+
+		// 定位图层初始化
+		myLocationOverlay = new MyLocationOverlay(mMapView);
+		// 设置定位数据
+		myLocationOverlay.setData(locData);
+		// 添加定位图层
+		mMapView.getOverlays().add(myLocationOverlay);
+		myLocationOverlay.enableCompass();
+		// 修改定位数据后刷新图层生效
+		mMapView.refresh();
 	}
 
+	/**
+	 * 手动触发一次定位请求
+	 */
+	public void requestLocClick() {
+		isRequest = true;
+		mLocClient.requestLocation();
+		Toast.makeText(this, "正在定位......", Toast.LENGTH_SHORT).show();
+	}
+
+	/**
+	 * 定位SDK监听函数
+	 */
+	public class MyLocationListenner implements BDLocationListener {
+
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			if (location == null)
+				return;
+
+			locData.latitude = location.getLatitude();
+			locData.longitude = location.getLongitude();
+			// 如果不显示定位精度圈，将accuracy赋值为0即可
+			locData.accuracy = location.getRadius();
+			locData.direction = location.getDerect();
+			// 更新定位数据
+			myLocationOverlay.setData(locData);
+			// 更新图层数据执行刷新后生效
+			mMapView.refresh();
+			// 是手动触发请求或首次定位时，移动到定位点
+			if (isRequest || isFirstLoc) {
+				// 移动地图到定位点
+				mMapController.animateTo(new GeoPoint(
+						(int) (locData.latitude * 1e6),
+						(int) (locData.longitude * 1e6)));
+				isRequest = false;
+			}
+			// 首次定位完成
+			isFirstLoc = false;
+		}
+
+		public void onReceivePoi(BDLocation poiLocation) {
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		mMapView.onPause();
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		mMapView.onResume();
+		super.onResume();
+	}
+
+	@Override
+	protected void onDestroy() {
+		// 退出时销毁定位
+		if (mLocClient != null)
+			mLocClient.stop();
+		mMapView.destroy();
+		super.onDestroy();
+	}
 }
