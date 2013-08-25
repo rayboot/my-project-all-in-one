3b9eb733e1fe33015554f7e425367f412f891561
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-08-25 14:28:16 +0000

    基础代码

---
+++ code/QChong/src/com/rayboot/qchong/OrderObj.java
@@ -0,0 +1,6 @@
+package com.rayboot.qchong;
+
+public class OrderObj {
+	public String code = "";
+	public String msg = "";
+}
---
+++ code/QChong/src/com/rayboot/qchong/SMSObj.java
@@ -0,0 +1,7 @@
+package com.rayboot.qchong;
+
+public class SMSObj {
+	public String code = "";
+	public String msg = "";
+	public String orderId = "";
+}
