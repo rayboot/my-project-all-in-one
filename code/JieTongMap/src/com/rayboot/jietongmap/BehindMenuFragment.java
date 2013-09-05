18de8dd1e4447ccd28f55578a83eaa2cd9cd42cd
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-09-05 09:21:17 +0000

    侧滑

---
+++ code/JieTongMap/src/com/rayboot/jietongmap/BehindMenuFragment.java
@@ -0,0 +1,19 @@
+package com.rayboot.jietongmap;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BehindMenuFragment extends Fragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.menu_behind, null);
+	}
+
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+	}
+}
