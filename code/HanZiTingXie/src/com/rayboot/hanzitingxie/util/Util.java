cc0c033358c2b6fc959daad223095400b216fbfe
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-10-12 07:10:17 +0000

    base

---
+++ code/HanZiTingXie/src/com/rayboot/hanzitingxie/util/Util.java
@@ -0,0 +1,66 @@
+package com.rayboot.hanzitingxie.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Bundle;
+
+public class Util {
+
+	// 获取AppKey
+	public static String getMetaValue(Context context, String metaKey) {
+		Bundle metaData = null;
+		String apiKey = null;
+		if (context == null || metaKey == null) {
+			return null;
+		}
+		try {
+			ApplicationInfo ai = context.getPackageManager()
+					.getApplicationInfo(context.getPackageName(),
+							PackageManager.GET_META_DATA);
+			if (null != ai) {
+				metaData = ai.metaData;
+			}
+			if (null != metaData) {
+				apiKey = metaData.getString(metaKey);
+			}
+		} catch (NameNotFoundException e) {
+
+		}
+		return apiKey;
+	}
+
+	public static boolean shareSomethingText(Context context, String title,
+			String content) {
+		Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.setType("text/plain"); // 分享发送到数据类型
+		intent.putExtra(Intent.EXTRA_SUBJECT, title); // 分享的主题
+		intent.putExtra(Intent.EXTRA_TEXT, content); // 附带的说明信息
+		context.startActivity(Intent.createChooser(intent, "分享"));
+		return true;
+	}
+
+	public static String getVersionName(Context context) {
+		// 获取packagemanager的实例
+		PackageManager packageManager = context.getPackageManager();
+		// getPackageName()是你当前类的包名，0代表是获取版本信息
+		PackageInfo packInfo;
+		String version = "1.0.0";
+		try {
+			packInfo = packageManager.getPackageInfo(context.getPackageName(),
+					0);
+			version = packInfo.versionName;
+		} catch (NameNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return version;
+	}
+
+}
