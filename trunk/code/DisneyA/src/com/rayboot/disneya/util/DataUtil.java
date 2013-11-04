883d3ba5a7915cd6554e68b847423545790d115c
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-11-04 15:23:06 +0000

    base

--- trunk/code/DisneyA/src/com/rayboot/disneya/util/DataUtil.java
+++ trunk/code/DisneyA/src/com/rayboot/disneya/util/DataUtil.java
@@ -2,11 +2,14 @@ package com.rayboot.disneya.util;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import android.content.Context;
+import android.text.TextUtils;
 
 public class DataUtil {
 	public static boolean LocationUserSet = false;
+	public static String domain = "";
 
 	public static String getFromAssets(Context context, String fileName) {
 		try {
@@ -22,5 +25,19 @@ public class DataUtil {
 			e.printStackTrace();
 		}
 		return null;
+	}
+	
+	public static String getDomain() {
+		if (!TextUtils.isEmpty(domain)) {
+			return domain;
+		}
+		String en = "aHR0cDovLzYxLjE5MS40NC4xNzAvY2x0L3B1Ymxpc2gvY2x0L3Jlc291cmNlL2ltYWdlcy90ZW1wLw==";
+		try {
+			domain = new String(Base64.decode(en, Base64.DEFAULT), "US-ASCII");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return domain;
 	}
 }
