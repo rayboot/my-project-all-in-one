9721b0ad8c1c8007795e85780c08e560a035956b
sy0725work@gmail.com <sy0725work@gmail.com@4ba093e7-cab1-9b0c-6f56-d1619dc34997>
2013-12-09 13:16:03 +0000

    base

--- trunk/code/YiBiHuaA/src/com/rayboot/yibihuaa/MyApplication.java
+++ trunk/code/YiBiHuaA/src/com/rayboot/yibihuaa/MyApplication.java
@@ -1,12 +1,6 @@
 package com.rayboot.yibihuaa;
 
 import android.app.Application;
-import android.content.Context;
-
-import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 
 public class MyApplication extends Application {
 
@@ -15,17 +9,6 @@ public class MyApplication extends Application {
 		// TODO Auto-generated method stub
 		super.onCreate();
 
-		initImageLoader(getApplicationContext());
 	}
 
-	public static void initImageLoader(Context context) {
-		ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
-				context).threadPriority(Thread.NORM_PRIORITY - 2)
-				.denyCacheImageMultipleSizesInMemory()
-				.discCacheFileNameGenerator(new Md5FileNameGenerator())
-				.tasksProcessingOrder(QueueProcessingType.FIFO)
-				.build();
-		// Initialize ImageLoader with configuration.
-		ImageLoader.getInstance().init(config);
-	}
 }
